cf585c2bae501d17d2b4172d0eb58783686f0072
trotlinebeercan <hopper.dustin@gmail.com>
2023-03-03 19:49:38 -0600

    Update client_postbuild.bat to use client_postbuild.py

--- Scripts/client_postbuild.bat
+++ Scripts/client_postbuild.bat
@@ -4,7 +4,7 @@ if errorlevel 1 goto errorNoPython
 
 :: Reaching here means Python is installed.
 :: Execute stuff...
-python %1/Scripts/postbuild.py -i "%1/YgoMaster/" -o "%ProgramFiles(x86)%/Steam/steamapps/common/Yu-Gi-Oh!  Master Duel/YgoMaster_Dev"
+python %1/Scripts/client_postbuild.py -i "%1/YgoMaster/" -o "%ProgramFiles(x86)%/Steam/steamapps/common/Yu-Gi-Oh!  Master Duel/YgoMaster_Dev"
 
 :: Once done, exit the batch file -- skips executing the errorNoPython section
 goto:eof
