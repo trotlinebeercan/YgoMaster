aefea050e398ca34bcc563257e0be94137dca8dd
trotlinebeercan <hopper.dustin@gmail.com>
2023-03-07 12:14:11 -0600

    ConsoleHelper: Fix display of logs within Client

--- YgoMasterClient/ConsoleHelper.cs
+++ YgoMasterClient/ConsoleHelper.cs
@@ -30,10 +30,13 @@ namespace YgoMasterClient
         [DllImport("user32.dll", SetLastError = true)]
         private static extern bool IsWindowVisible(IntPtr hWnd);
 
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetConsoleOutputCP();
+
         private const UInt32 StdOutputHandle = 0xFFFFFFF5;
 
         private static IntPtr consoleHandle;
-        private static TextWriter output;
+        private static StreamWriter output;
 
         private const int SW_SHOW = 5;
         private const int SW_HIDE = 0;
@@ -57,20 +60,27 @@ namespace YgoMasterClient
 
         static ConsoleHelper()
         {
-            output = new ConsoleTextWriter();
-            Console.SetOut(output);
-
             consoleHandle = GetConsoleWindow();
             if (consoleHandle != IntPtr.Zero)
             {
                 Console.Title = Title;
+
+                Stream consoleOutputStream = Console.OpenStandardOutput();
+                Encoding consoleEncoding = Encoding.GetEncoding(GetConsoleOutputCP());
+                output = new StreamWriter(consoleOutputStream, consoleEncoding, 0x100);
+                output.AutoFlush = true;
+
+                // SuppressFinalize on m_consoleOutputWriter because all it will do is flush
+                // and close the file handle. Because we have set AutoFlush the additional flush
+                // is not required. The console file handle should not be closed, so we don't call
+                // Dispose, Close or the finalizer.
+                GC.SuppressFinalize(output);
             }
         }
 
         public static bool IsConsoleVisible
         {
             get { return (consoleHandle = GetConsoleWindow()) != IntPtr.Zero && IsWindowVisible(consoleHandle); }
-            //get { return (consoleHandle = GetConsoleWindow()) != IntPtr.Zero; }
         }
 
         public static void ToggleConsole()
@@ -122,59 +132,5 @@ namespace YgoMasterClient
                 FreeConsole();
             }
         }
-    }
-
-    public class ConsoleTextWriter : TextWriter
-    {
-        public override Encoding Encoding { get { return Encoding.UTF8; } }
-
-        // TODO: WriteConsole may not write all the data, chunk this data into several calls if nessesary
-
-        // WriteConsoleW issues reference:
-        // https://svn.apache.org/repos/asf/logging/log4net/tags/log4net-1_2_9/src/Appender/ColoredConsoleAppender.cs
-
-        public override void Write(string value)
-        {
-            uint written;
-            if (!WriteConsoleW(new IntPtr(7), value, (uint)value.Length, out written, IntPtr.Zero) || written < value.Length)
-            {
-                if (GetConsoleWindow() != IntPtr.Zero)
-                {
-                    //System.Diagnostics.Debugger.Break();
-                }
-            }
-        }
-
-        public override void WriteLine(string value)
-        {
-            value = value + Environment.NewLine;
-            uint written;
-            if (!WriteConsoleW(new IntPtr(7), value, (uint)value.Length, out written, IntPtr.Zero) || written < value.Length)
-            {
-                if (GetConsoleWindow() != IntPtr.Zero)
-                {
-                    //System.Diagnostics.Debugger.Break();
-                }
-            }
-        }
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        static extern bool WriteConsoleW(IntPtr hConsoleOutput, [MarshalAs(UnmanagedType.LPWStr)] string lpBuffer,
-           uint nNumberOfCharsToWrite, out uint lpNumberOfCharsWritten,
-           IntPtr lpReserved);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        static extern bool WriteConsole(IntPtr hConsoleOutput, string lpBuffer,
-           uint nNumberOfCharsToWrite, out uint lpNumberOfCharsWritten,
-           IntPtr lpReserved);
-
-        [DllImport("kernel32.dll")]
-        static extern bool SetConsoleCP(int wCodePageID);
-
-        [DllImport("kernel32.dll")]
-        static extern uint GetACP();
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
     }
 }
